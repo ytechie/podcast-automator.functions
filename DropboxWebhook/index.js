f2180fbef115279168ece5bedc5ad7caf6962604
Jason Young <jason@ytechie.com>
2016-12-27 20:16:49 -0800

    Don't quote response - dropbox no likey

--- DropboxWebhook/index.js
+++ DropboxWebhook/index.js
@@ -2,16 +2,24 @@
     let challenge = req.query['challenge'];
 
     if (challenge) {
-        context.log('Got a challenge call');
+        context.log(`Got a challenge call: ${challenge}`);
+
         res = {
-            body: challenge
+            body: challenge,
+            headers: {
+                'Content-Type': 'text/plain'
+            }
         };
     }
     else {
         context.log('Got a regular call');
+
         res = {
             status: 200,
-            body: "got a regular call"
+            body: "got a regular call",
+            headers: {
+                'Content-Type': 'text/plain'
+            }
         };
     }
     context.done(null, res);
