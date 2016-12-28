7759578251400640b4c4ef18d248fe38565939a6
Jason Young <jason@ytechie.com>
2016-12-27 19:34:00 -0800

    initial commit

---
+++ DropboxWebhook/index.js
@@ -0,0 +1,18 @@
+﻿module.exports = function (context, req) {
+    let challenge = req.query['challenge'];
+
+    if (challenge) {
+        context.log('Got a challenge call');
+        res = {
+            body: challenge
+        };
+    }
+    else {
+        context.log('Got a regular call');
+        res = {
+            status: 200,
+            body: "got a regular call"
+        };
+    }
+    context.done(null, res);
+};
